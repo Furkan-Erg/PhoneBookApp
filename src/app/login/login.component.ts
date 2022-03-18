fc3de84d36c2268b1c2cd5456efe9ba40c0d9033
Batuhan GÜNDAY <batuhangunday@outlook.com>
2022-03-18 16:49:15 +0300

    Email format validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { LoginRequest } from '../models/auth/login/LoginRequest';
 import { AuthService } from '../services/auth/auth.service';
 
@@ -17,11 +18,16 @@ export class LoginComponent implements OnInit {
     private readonly formBuilder: FormBuilder,
     private readonly router:Router
   ) {}
-
+  get Email(){
+    return this.loginForm.get('Email')
+    }
+  get Password(){
+    return this.loginForm.get('Password')
+  }
   prepareForm() {
     this.loginForm = this.formBuilder.group({
-      Email: [null],
-      Password: [null],
+      Email: [null,[Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      Password: [null,[Validators.required,]],
     });
   }
   ngOnInit(): void {
@@ -37,9 +43,11 @@ export class LoginComponent implements OnInit {
       if(e.success){
           localStorage.setItem("userId",e.userId.toString())
           this.router.navigate(["/contact"])
-
-      };
-
+      } else {
+        Swal.fire(
+          'Your Email Or Password is wrong'
+        );
+      }
     });
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,15 +21,23 @@ export class RegisterComponent implements OnInit {
 
   prepareForm() {
     this.registerForm = this.formBuilder.group({
-      Name: [null],
-      Surname:[null],
-      Email: [null],
+      Name: [null,[Validators.required,Validators.pattern("^[^0-9 ]+$")]],
+      Surname:[null,[Validators.required,Validators.pattern("^[^0-9 ]+$")]],
+      Email: [null,[Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       Password: [null],
       TermsOfService:[false,[Validators.requiredTrue]]
     });
   }
   ngOnInit(): void {
     this.prepareForm();
+  }
+
+  get Email(){
+    return this.registerForm.get('Email')
+    }
+
+  get Password(){
+    return this.registerForm.get('Password')
   }
 
   registerButtonOnTap() {
